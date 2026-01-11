9f05264efa36f6441b5775c7f8bfaa99675fe802
HeinrichderAchte <henri.m.weber@gmail.com>
2026-01-11 12:05:55 +0100

    Fix namespace typo in ReservationsErrorTests

--- UnitTests/ReservationsErrorTests.cs
+++ UnitTests/ReservationsErrorTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Biletado.Services;
-using Biletado.Respository;
+using Biletado.Repository;
 using Biletado.Persistence.Contexts;
 using FluentAssertions;
 using Moq;
